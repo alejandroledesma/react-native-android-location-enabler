5fa09554e755e725554373aa94b0e4af3ea7067a
alejandroledesma <alejandro@waavi.com>
2018-04-03 12:39:52 +0200

    Delete @Override in RNAndroidLocationEnablerPackage, Deprecated in RN >= 47

--- android/src/main/java/com/heanoria/library/reactnative/locationenabler/RNAndroidLocationEnablerPackage.java
+++ android/src/main/java/com/heanoria/library/reactnative/locationenabler/RNAndroidLocationEnablerPackage.java
@@ -11,18 +11,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class RNAndroidLocationEnablerPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Collections.<NativeModule>singletonList(new RNAndroidLocationEnablerModule(reactContext));
-    }
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Collections.<NativeModule>singletonList(new RNAndroidLocationEnablerModule(reactContext));
+  }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
-    }
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
 }
